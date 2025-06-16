55b6a649c235ef2f2e908003cb5b9f8d4a733f9f
johnf008 <Carlos.flores.h108@gmail.com>
2025-06-15 19:33:46 -0500

    Set the function outlines & figured out what i need to do to convert my js to jsx

--- react-convert/src/components/Home.jsx
+++ react-convert/src/components/Home.jsx
@@ -1,19 +1,42 @@
+import React, {useState, useEffect} from "react";
 
 function Home() {
+
+    const initialTime = 1500;
+    const [timeLeft, updateTime] = useState(initialTime);
+    
+    function start() {
+
+    }
+
+    function stop() {
+
+    }
+
+    function reset() {
+
+    }
+
+    function formatTime(){
+        return '25:00';
+    }
+
+
+    
     return (
     <>
     <div className="flex-1 flex items-center justify-center mt-6">
 
     <div className=" text-red-950 font-bold text-center">
         <div className="min-h-[50px] text-3xl ">Pomodoro Pets</div>
-        <div className="min-h-[100px] text-9xl" id="timer">25:00</div>
+        <div className="min-h-[100px] text-9xl" id="timer">{formatTime()}</div>
 
         
 
         <div className="mt-3">
-            <button className="mx-2 cursor-pointer text-white bg-green-500 hover:bg-green-400 py-2 px-4 border-b-4 border-gray-50 hover:border-gray-100 rounded" id="start">Start</button>
-            <button className="mx-2 cursor-pointer text-white bg-red-500 hover:bg-red-400 py-2 px-4 border-b-4 border-gray-50 hover:border-gray-100 rounded" id="stop">Stop</button>
-            <button className="mx-2 cursor-pointer text-white bg-yellow-500 hover:bg-yellow-400 py-2 px-4 border-b-4 border-gray-50 hover:border-gray-100 rounded" id="reset">Reset</button>
+            <button onClick={start} className="mx-2 cursor-pointer text-white bg-green-500 hover:bg-green-400 py-2 px-4 border-b-4 border-gray-50 hover:border-gray-100 rounded">Start</button>
+            <button onClick={stop}className="mx-2 cursor-pointer text-white bg-red-500 hover:bg-red-400 py-2 px-4 border-b-4 border-gray-50 hover:border-gray-100 rounded">Stop</button>
+            <button onClick={reset} className="mx-2 cursor-pointer text-white bg-yellow-500 hover:bg-yellow-400 py-2 px-4 border-b-4 border-gray-50 hover:border-gray-100 rounded">Reset</button>
         </div>
     </div>
 
---
+++ react-convert/src/scripts/index.js
@@ -0,0 +1,137 @@
+//Hi :3
+const startEl = document.getElementById("start");
+const stopEl = document.getElementById("stop");
+const resetEl = document.getElementById("reset");
+
+const timer = document.getElementById("timer");
+
+const levelEl = document.getElementById("level");
+const pomodorosEl = document.getElementById("pomodoros");
+
+let interval;
+let remainingTime = 1500;
+let intervalCheck = false;
+
+let pomodoros;
+let levels; 
+let leftSide = "";
+let rightSide = "";
+let char;
+let rightSideCheck = false;
+
+let sections; 
+
+function updateTimer(){
+    let mins = Math.floor(remainingTime / 60);
+    let secs = remainingTime % 60;
+
+    mins = mins.toString();
+    secs = secs.toString();
+
+    if (mins.length < 2){
+        mins = "0" + mins;
+        
+    }
+
+    if (secs.length < 2){
+        secs = "0" + secs;
+    }
+
+    let newTime = mins + ":" + secs;
+
+    timer.innerHTML = newTime;
+}
+
+function updateLevel(){
+    pomodoros = pomodorosEl.innerHTML;
+    levels = levelEl.innerHTML; 
+
+    levels = parseInt(levels);
+    //sections = pomodoros.split("/");
+
+    //leftSide = sections[0]
+    //rightSide = sections[1]
+
+    for (let i = 0; i < pomodoros.length; i++){
+        char = pomodoros.charAt(i);
+        console.log("Char: " + char);
+
+        if(char === "/" || rightSideCheck){
+            if(rightSideCheck){
+                rightSide += char.toString();
+            }
+            else{
+                rightSideCheck = true
+            }
+        }
+        else{
+            leftSide += char.toString();
+        }
+        console.log("Left: "+ leftSide);
+        console.log("Right: " + rightSide);
+    }
+
+        leftSide = parseInt(leftSide);
+        rightSide = parseInt(rightSide);
+
+        leftSide += 1; 
+
+        if (leftSide / rightSide === 1){
+            leftSide = 0;
+            rightSide += 1; 
+
+            levels += 1; 
+
+            leftSide = leftSide.toString();
+            rightSide = rightSide.toString();
+            levels = levels.toString();
+
+
+            levelEl.innerHTML = levels;
+            pomodorosEl.innerHTML = leftSide + "/" + rightSide;
+        }
+    
+}
+function startTimer(){
+    if(intervalCheck) return;
+    document.getElementById("cat_image").src = "images/studious_cat.png";
+    intervalCheck = true;
+    startEl.disabled = true; 
+    interval = setInterval(()=>{
+        remainingTime = remainingTime - 1;
+        updateTimer();
+
+        if(remainingTime === 0){
+            clearInterval(interval);
+            
+            setTimeout(()=> {
+            alert("Time's up! Take a break!");
+            remainingTime = 1500;
+            intervalCheck = false;
+            startEl.disabled = false;
+            updateLevel();
+            document.getElementById("cat_image").src = "images/rest_cat.jpg";
+            }, 0);
+        }
+    }, 1000)
+}
+
+function stopTimer(){
+    if (!intervalCheck) return;
+    clearInterval(interval);
+    intervalCheck = false;
+    startEl.disabled = false; 
+    
+}
+
+function resetTimer(){
+    clearInterval(interval);
+    remainingTime = 1500;
+    intervalCheck = false;
+    startEl.disabled = false; 
+    updateTimer();
+}
+
+startEl.addEventListener("click", startTimer);
+stopEl.addEventListener("click", stopTimer);
+resetEl.addEventListener("click", resetTimer);
