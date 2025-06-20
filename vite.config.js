95e3bee52cb7d74dcd339733cee1a358ef3be990
johnf008 <Carlos.flores.h108@gmail.com>
2025-06-20 15:35:02 -0500

    Fixed base & strick mode to fix deployment

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "Pomodoro_Pet",
+  base: "/Pomodoro_Pet",
 })
